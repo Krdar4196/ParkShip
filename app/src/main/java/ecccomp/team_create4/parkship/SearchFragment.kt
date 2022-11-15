37dc785617bc3a0184d1530c24abd853659e33c4
Naoki-Kuroda-4196 <2200418@ecccomp.com>
2022-11-15 11:37:25 +0900

    下部ナビゲーションにマップ、検索、マイページ、設定を追加しました。 各自用意したフラグメントで作業を行ってください。

--- app/src/main/java/ecccomp/team_create4/parkship/SearchFragment.kt
+++ app/src/main/java/ecccomp/team_create4/parkship/SearchFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BottomNavBarFragment.newInstance] factory method to
+ * Use the [SearchFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BottomNavBarFragment : Fragment() {
+class SearchFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class BottomNavBarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_main, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class BottomNavBarFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BottomNavBarFragment.
+         * @return A new instance of fragment SearchFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BottomNavBarFragment().apply {
+            SearchFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
