b6692d0c6013a2cf9d46a2895d816cae3a080f32
aruvino99 <84892759+aruvino99@users.noreply.github.com>
2022-11-24 14:52:40 +0900

    Revert "Feature 01 setting"

--- app/src/main/java/ecccomp/team_create4/parkship/MainActivity.kt
+++ app/src/main/java/ecccomp/team_create4/parkship/MainActivity.kt
@@ -11,43 +11,22 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var navigation: BottomNavigationView
 
-    /**
-     * 下部ナビゲーションのアイコンを選択した時の処理
-     * マップ      : 公園の場所が分かる Google Map フラグメント
-     * 検索        : 公園の検索ができるフラグメント
-     * マイページ   : ログインユーザーの情報が表示されるフラグメント
-     * 設定        : 各設定が行えるフラグメント
-     */
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId){
-            //マップ
             R.id.menu1 -> {
-                /** エラー起こるのでコメントアウトしてます **/
-//                supportFragmentManager.beginTransaction()
-//                    .replace(R.id.frameLayout, map())
-//                    .commit()
-//                return@OnNavigationItemSelectedListener true
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout, ExampleFragment())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
             }
-            //検索
             R.id.menu2 -> {
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.frameLayout, SearchFragment())
+                    .replace(R.id.frameLayout, map())
                     .commit()
                 return@OnNavigationItemSelectedListener true
             }
-            //マイページ
             R.id.menu3 -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.frameLayout, MyPageFragment())
-                    .commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            //設定
-            R.id.menu4 -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.frameLayout, SettingFragment())
-                    .commit()
-                return@OnNavigationItemSelectedListener true
+
             }
         }
         return@OnNavigationItemSelectedListener false
