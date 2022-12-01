e600d9e2d66bbb7cc6b0d6a118c5772f1bd51a60
HERUBA005 <am0nn0120@gmail.com>
2022-12-01 16:25:55 +0900

    ログイン画面バインディング実装済み

--- app/src/main/java/ecccomp/team_create4/parkship/LoginActivity.kt
+++ app/src/main/java/ecccomp/team_create4/parkship/LoginActivity.kt
@@ -1,11 +1,55 @@
 package ecccomp.team_create4.parkship
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
+import ecccomp.team_create4.parkship.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    //バインディング読み込み
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        //追加
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        //変更
+        //setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        /*(ボタン)-------------------------------------------------------*/
+        binding.button01.setOnClickListener {
+            //送る
+            //ログイン画面を呼び出すIntentを生成
+            val intent = Intent(application, LoginActivity::class.java)
+            //Intent呼び出しを実行する
+            startActivity(intent)
+            //トースト
+            val msg = "LOGIN"
+            Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG).show()
+
+            /*
+            val ve = "VERBOSE/冗長"
+            Log.v(ContentValues.TAG,ve)
+
+            val de = "DEBUG/デバック"
+            Log.d(ContentValues.TAG,de)
+
+            val inf = "INFORMATION/情報"
+            Log.i(ContentValues.TAG,inf)
+
+            val wa = "WARNING/警告"
+            Log.w(ContentValues.TAG,wa)
+
+            val er = "ERROR/エラー"
+            Log.e(ContentValues.TAG,er)
+            */
+        }
+        /*-------------------------------------------------------(ボタン)*/
+
     }
 }
