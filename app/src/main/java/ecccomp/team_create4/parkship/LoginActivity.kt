d2eb5fb11b3d61dbb7867341b97dd9bb2f2110b1
HERUBA005 <am0nn0120@gmail.com>
2022-12-01 14:47:55 +0900

    ログイン画面の再構築

---
+++ app/src/main/java/ecccomp/team_create4/parkship/LoginActivity.kt
@@ -0,0 +1,11 @@
+package ecccomp.team_create4.parkship
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
