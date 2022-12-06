9f155516982687ad7532ee7349361a01d09bafe6
Krdar4196 <94659345+Naoki-Kuroda-4196@users.noreply.github.com>
2022-12-06 12:11:25 +0900

    Revert "Dev 02"

--- app/src/main/java/ecccomp/team_create4/parkship/map.kt
+++ app/src/main/java/ecccomp/team_create4/parkship/map.kt
@@ -1,26 +1,39 @@
 package ecccomp.team_create4.parkship
 
 import android.os.Bundle
-import android.view.Gravity.apply
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.GravityCompat.apply
-import com.google.android.gms.maps.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
+class map : Fragment() {
 
-class map : Fragment(), OnMapReadyCallback {
+    private var param1: String? = null
+    private var param2: String? = null
 
-    lateinit var map: MapView
-    lateinit var mMap: GoogleMap
+    lateinit var MapView: MapView
+    lateinit var mMapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requireActivity().setTitle("Park Map!!!")
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //MapView.onStart()
     }
 
     override fun onCreateView(
@@ -29,41 +42,42 @@ class map : Fragment(), OnMapReadyCallback {
     ): View? {
         // Inflate the layout for this fragment
         val fragmentView =  inflater.inflate(R.layout.fragment_map, container, false)
-        //map = fragmentView.findViewById(R.id.mapView)
+        MapView = fragmentView.findViewById(R.id.mapView)
         return fragmentView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val mapFragment =
-            childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-        mMap.setOnInfoWindowClickListener {
-
+        mMapView = MapView.apply {
+            onCreate(savedInstanceState)
+            //getMapAsync(this@CustomMapFragment)
         }
     }
 
-//    inner class CustomMapFragment : OnMapReadyCallback{
-//        override fun onMapReady(googleMap: GoogleMap) {
-//            mMap = googleMap
-//
-//            // Add a marker in Sydney and move the camera
-//            val sydney = LatLng(-34.0, 151.0)
-//            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-//
-//            mMap.setOnInfoWindowClickListener {
-//
-//            }
-//        }
-//    }
+    inner class CustomMapFragment : OnMapReadyCallback{
+        private lateinit var mMap: GoogleMap
+        override fun onMapReady(googleMap: GoogleMap) {
+            mMap = googleMap
+
+            // Add a marker in Sydney and move the camera
+            val sydney = LatLng(-34.0, 151.0)
+            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+            mMap.setOnInfoWindowClickListener {
+
+            }
+        }
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            map().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
