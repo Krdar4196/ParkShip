6c83365073584859579fbe85f5981aaf9f02e43a
HERUBA005 <am0nn0120@gmail.com>
2022-12-08 15:13:05 +0900

    Login画面の初期設定

--- app/src/main/java/ecccomp/team_create4/parkship/LoginActivity.kt
+++ app/src/main/java/ecccomp/team_create4/parkship/LoginActivity.kt
@@ -1,4 +1,25 @@
 package ecccomp.team_create4.parkship
 
-class LoginActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.content.ContentValues
+import android.widget.Toast
+import ecccomp.team_create4.parkship.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        //追記
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        //変更
+        //setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+    }
+
 }
