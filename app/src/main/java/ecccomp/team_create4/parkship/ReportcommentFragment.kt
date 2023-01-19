993783b98d71cbb691c39fc021d547b47718b950
aruvino99 <aruvino99@gmail.com>
2023-01-19 14:36:47 +0900

    ParkDetailFragment.ktの枠組みと画面遷移を作成しました

--- app/src/main/java/ecccomp/team_create4/parkship/ReportcommentFragment.kt
+++ app/src/main/java/ecccomp/team_create4/parkship/ReportcommentFragment.kt
@@ -1,15 +1,10 @@
 package ecccomp.team_create4.parkship
 
 import android.os.Bundle
-import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.ReportFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ParkDetailFragment.newInstance] factory method to
+ * Use the [ReportcommentFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ParkDetailFragment : Fragment() {
+class ReportcommentFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -39,21 +34,7 @@ class ParkDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_park_detail, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val parkdetailNametext: TextView = view.findViewById(R.id.parkdetailNametext)
-        val parkdetailAddresstext: TextView = view.findViewById(R.id.parkdetailAddresstext)
-        val parkdetailReporttext: TextView = view.findViewById(R.id.parkdetailReporttext)
-        val parkdetailReportbutton: Button = view.findViewById(R.id.parkdetailReportbutton)
-        // ボタンのリスナーを作成
-        parkdetailReportbutton.setOnClickListener {
-            childFragmentManager.beginTransaction()
-                .replace(R.id.container,ReportcommentFragment())
-                .commit()
-        }
+        return inflater.inflate(R.layout.fragment_reportcomment, container, false)
     }
 
     companion object {
@@ -63,12 +44,12 @@ class ParkDetailFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ParkDetailFragment.
+         * @return A new instance of fragment ReportcommentFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ParkDetailFragment().apply {
+            ReportcommentFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
