8fe13edb8986319aae5eaaee0123341dedf776bb
Naoki-Kuroda-4196 <2200418@ecccomp.com>
2023-01-24 22:57:16 +0900

    マイページの枠組み作成 : 微調整

--- app/src/main/java/ecccomp/team_create4/parkship/MyPageFragment.kt
+++ app/src/main/java/ecccomp/team_create4/parkship/MyPageFragment.kt
@@ -1,10 +1,10 @@
 package ecccomp.team_create4.parkship
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class MyPageFragment : Fragment() {
 
@@ -22,7 +22,6 @@ class MyPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
 
     }
 }
